b5f148950b21fb4c85acd68e65f941bdd7e0e0bf
Morgan Spencer <morgan@spencercreative.co>
2022-09-18 08:20:48 -0400

    Add salt to all crypto functions

--- src/crypto.test.ts
+++ src/crypto.test.ts
@@ -2,25 +2,47 @@ import { decrypt, encrypt, hash } from '.'
 
 const value = 'I am a value to test'
 const secret = '8b211be1-bc73-4806-836a-ead4c14c48b7'
+const salt = 'cl879zjuf000009mda8lub0fi'
+const altSalt = 'cl87ahy2o000009mvh93ccaod'
 
-describe('utilities', () => {
-  const encrypted = encrypt(value, secret)
-  const decrypted = decrypt(encrypted, secret)
+describe('Crypto', () => {
+  const encrypted = encrypt(value, secret, salt)
+  const altEncrypted = encrypt(value, secret, altSalt)
+  const decrypted = decrypt(encrypted, secret, salt)
+  const altDecrypted = decrypt(altEncrypted, secret, altSalt)
 
   it('should encrypt', () => {
-    expect(encrypted).toEqual(
-      'e2e73a777fad4b384b3a2ba591898ad147eee45db9886606584779dbb3941fb4'
+    expect(encrypted).toBe(
+      'cc7a8649dffe80ce33c10fc8194003bdcfafc4d88a896029bf8824265be77bdd'
+    )
+
+    expect(altEncrypted).toBe(
+      '26411fbb989f4192405dd00008a76c695e3c4c820ed2dc280107b313ea7a6ec5'
     )
   })
 
   it('should decrypt', () => {
-    expect(decrypted).toEqual(value)
+    expect(decrypted).toBe(value)
+    expect(altDecrypted).toBe(value)
+  })
+
+  it('should salt', () => {
+    expect(encrypted).not.toBe(altEncrypted)
   })
 
   it('should hash', () => {
-    const one = hash(value)
-    const two = hash(value)
+    const one = hash(value, salt)
+    const two = hash(value, salt)
+    const three = hash(value, altSalt)
+
+    expect(one).toBe(
+      '6838a432d794902aabf6c7c5d8eccd77ddd39b67a1f06ebdad584ef06b90fe03'
+    )
+    expect(three).toBe(
+      '311746c9128e4dd6b378b9fba589af403ca959ee363788f24d1cee1b05a1574e'
+    )
 
     expect(one).toBe(two)
+    expect(one).not.toBe(three)
   })
 })
--- src/crypto.ts
+++ src/crypto.ts
@@ -6,22 +6,30 @@ const inEnc = 'hex'
 const outEnc = 'utf8'
 const iv = Buffer.alloc(16)
 
-const decrypt = (value: string, secret: string) => {
-  const key = crypto.createHash(hashAlgo).update(secret).digest()
+const generateBuffer = (value: string) => {
+  return Buffer.from(value, outEnc)
+}
+
+const generateKey = (secret: string, salt: string) => {
+  return crypto.scryptSync(generateBuffer(secret), generateBuffer(salt), 32)
+}
+
+const decrypt = (value: string, secret: string, salt: string) => {
+  const key = generateKey(secret, salt)
   const decipher = crypto.createDecipheriv(encryptAlgo, key, iv)
 
   return decipher.update(value, inEnc, outEnc) + decipher.final(outEnc)
 }
 
-const encrypt = (value: string, secret: string) => {
-  const key = crypto.createHash(hashAlgo).update(secret).digest()
+const encrypt = (value: string, secret: string, salt: string) => {
+  const key = generateKey(secret, salt)
   const cipher = crypto.createCipheriv(encryptAlgo, key, iv)
 
   return cipher.update(value, outEnc, inEnc) + cipher.final(inEnc)
 }
 
-const hash = (value: string) => {
-  return crypto.createHash(hashAlgo).update(value).digest(inEnc)
+const hash = (value: string, salt: string) => {
+  return crypto.createHash(hashAlgo).update(generateBuffer(value)).update(generateBuffer(salt)).digest(inEnc)
 }
 
 export { decrypt, encrypt, hash }
